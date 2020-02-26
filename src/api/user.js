44b043d062f996fab2ac1fb9a1ee9b85bceb5096
BooksCup <812809680@qq.com>
2020-02-26 15:38:13 +0800

    first commit

---
+++ src/api/user.js
@@ -0,0 +1,40 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    url: '/vue-element-admin/user/login',
+    method: 'post',
+    data
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/vue-element-admin/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/vue-element-admin/user/logout',
+    method: 'post'
+  })
+}
+
+export function fetchUserList(query) {
+  return request({
+    url: 'http://localhost:8080/users',
+    method: 'get',
+    params: query
+  })
+}
+
+export function createUser(data) {
+  return request({
+    url: 'http://localhost:8080/users',
+    method: 'post',
+    params: data
+  })
+}
