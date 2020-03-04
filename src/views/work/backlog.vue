30f7421e48a0ddd153fa053658c878702db5b3bd
BooksCup <812809680@qq.com>
2020-03-04 15:11:41 +0800

    完善backlog新增表单的联动功能

--- src/views/work/backlog.vue
+++ src/views/work/backlog.vue
@@ -15,7 +15,7 @@
         class="filter-item"
         style="margin-left: 10px;"
         type="primary"
-        icon="el-icon-edit"
+        icon="el-icon-document-add"
         @click="handleCreate"
       >
         添加
@@ -26,7 +26,7 @@
       <el-table-column align="left" width="370px" label="标题">
         <template slot-scope="scope">
           <el-tag :type="scope.row.type | typeTagFilter">{{ scope.row.type | typeFilter }}</el-tag>
-          <span>{{ scope.row.title }}</span>
+          <span>【{{ scope.row.moduleName }}】{{ scope.row.title }}</span>
         </template>
       </el-table-column>
 
@@ -75,8 +75,8 @@
 
       <el-table-column align="center" min-width="100px" label="操作">
         <template slot-scope="{row, $index}">
-          <el-button size="small" type="primary" icon="el-icon-edit" @click="handleUpdate(row, $index)" />
-          <el-button size="small" type="danger" icon="el-icon-delete" @click="handleDelete(row, $index)" />
+          <el-button size="small" type="primary" icon="el-icon-edit" @click="handleUpdate(row, $index)"/>
+          <el-button size="small" type="danger" icon="el-icon-delete" @click="handleDelete(row, $index)"/>
         </template>
       </el-table-column>
     </el-table>
@@ -95,50 +95,66 @@
         :rules="createUserRules"
         :model="temp"
         label-position="left"
-        label-width="120px"
+        label-width="150px"
         style="width: 400px; margin-left:50px;"
       >
         <el-form-item label="类型" prop="type">
-          <el-select v-model="temp.type" class="filter-item" placeholder="">
-            <el-option v-for="item in typeOptions" :key="item.value" :label="item.label" :value="item.value" />
+          <el-select v-model="temp.type" class="filter-item">
+            <el-option v-for="item in typeOptions" :key="item.value" :label="item.label" :value="item.value"/>
           </el-select>
         </el-form-item>
         <el-form-item label="标题" prop="title">
-          <el-input v-model="temp.title" placeholder="请输入标题" />
+          <el-input v-model="temp.title" placeholder="请输入标题"/>
         </el-form-item>
         <el-form-item label="状态" prop="statusId">
           <el-select v-model="temp.statusId" class="filter-item" placeholder="">
-            <el-option v-for="item in statusOptions" :key="item.id" :label="item.name" :value="item.id" />
+            <el-option v-for="item in statusOptions" :key="item.id" :label="item.name" :value="item.id"/>
           </el-select>
         </el-form-item>
         <el-form-item label="处理人" prop="currentUserId">
           <el-select v-model="temp.currentUserId" class="filter-item" placeholder="">
-            <el-option v-for="item in currentUserOptions" :key="item.id" :label="item.name" :value="item.id" />
+            <el-option v-for="item in currentUserOptions" :key="item.id" :label="item.name" :value="item.id"/>
           </el-select>
         </el-form-item>
         <el-form-item label="模块" prop="moduleId">
           <el-select v-model="temp.moduleId" class="filter-item" placeholder="">
-            <el-option v-for="item in moduleOptions" :key="item.id" :label="item.name" :value="item.id" />
+            <el-option v-for="item in moduleOptions" :key="item.id" :label="item.name" :value="item.id"/>
           </el-select>
         </el-form-item>
         <el-form-item label="迭代" prop="sprintId">
-          <el-select v-model="temp.sprintId" class="filter-item" placeholder="">
-            <el-option v-for="item in sprintOptions" :key="item.id" :label="item.name" :value="item.id" />
+          <el-select v-model="temp.sprintId" class="filter-item" placeholder="" @change="choseSprint" clearable>
+            <el-option v-for="item in sprintOptions" :key="item.id" :label="item.name" :value="item.id"/>
           </el-select>
+        </el-form-item>
+        <el-form-item label="是否联动预计开始结束日期？" prop="isLinkSprint" v-show="showIsLinkSprint">
+          <el-radio-group v-model="temp.isLinkSprint" @change="linkSprint">
+            <el-radio :label='"0"'>
+              否
+            </el-radio>
+            <el-radio :label='"1"'>
+              是
+            </el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="预计开始日期" prop="beginDate">
+          <el-date-picker v-model="temp.beginDate" format="yyyy-MM-dd" value-format="yyyy-MM-dd" placeholder="请选择开始日期"/>
+        </el-form-item>
+        <el-form-item label="预计结束日期" prop="endDate">
+          <el-date-picker v-model="temp.endDate" format="yyyy-MM-dd" value-format="yyyy-MM-dd" placeholder="请选择结束日期"/>
         </el-form-item>
         <el-form-item label="优先级顺序" prop="priorityOrder">
           <el-select v-model="temp.priorityOrder" class="filter-item" placeholder="">
-            <el-option v-for="item in priorityOrderOptions" :key="item.value" :label="item.label" :value="item.value" />
+            <el-option v-for="item in priorityOrderOptions" :key="item.value" :label="item.label" :value="item.value"/>
           </el-select>
         </el-form-item>
         <el-form-item label="优先级" prop="priority">
           <el-select v-model="temp.priority" class="filter-item" placeholder="">
-            <el-option v-for="item in priorityOptions" :key="item.value" :label="item.label" :value="item.value" />
+            <el-option v-for="item in priorityOptions" :key="item.value" :label="item.label" :value="item.value"/>
           </el-select>
         </el-form-item>
         <el-form-item label="重要程度" prop="importance">
           <el-select v-model="temp.importance" class="filter-item" placeholder="">
-            <el-option v-for="item in importanceOptions" :key="item.value" :label="item.label" :value="item.value" />
+            <el-option v-for="item in importanceOptions" :key="item.value" :label="item.label" :value="item.value"/>
           </el-select>
         </el-form-item>
       </el-form>
@@ -161,23 +177,15 @@
         style="width: 400px; margin-left:50px;"
       >
         <el-form-item label="用户名" prop="name">
-          <el-input v-model="temp.name" :disabled="true" />
+          <el-input v-model="temp.name" :disabled="true"/>
         </el-form-item>
         <el-form-item label="手机号" prop="phone">
-          <el-input v-model="temp.phone" :disabled="true" />
+          <el-input v-model="temp.phone" :disabled="true"/>
         </el-form-item>
         <el-form-item label="邮箱" prop="mail">
-          <el-input v-model="temp.mail" :disabled="true" />
+          <el-input v-model="temp.mail" :disabled="true"/>
         </el-form-item>
         <el-form-item label="状态" prop="status">
-          <el-select v-model="temp.status">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            />
-          </el-select>
         </el-form-item>
         <el-form-item label="描述">
           <el-input
@@ -201,17 +209,17 @@
 </template>
 
 <script>
-  import { createUser, fetchUserList, deleteUser, updateUser } from '@/api/user'
-  import { fetchOptions } from '@/api/options'
-  import { createBacklog, fetchBacklogList } from '@/api/backlog'
+  import {createUser, fetchUserList, deleteUser, updateUser} from '@/api/user'
+  import {fetchOptions} from '@/api/options'
+  import {createBacklog, fetchBacklogList} from '@/api/backlog'
   import waves from '@/directive/waves' // waves directive
   import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
 
   // type
   // story/bug
   const typeOptions = [
-    { value: '0', label: 'story' },
-    { value: '1', label: 'bug' }
+    {value: '0', label: 'story'},
+    {value: '1', label: 'bug'}
   ]
 
   const typeKeyValue = typeOptions.reduce((acc, cur) => {
@@ -221,9 +229,9 @@
 
   // 优先级
   const priorityOptions = [
-    { value: '0', label: '低' },
-    { value: '1', label: '中' },
-    { value: '2', label: '高' }
+    {value: '0', label: '低'},
+    {value: '1', label: '中'},
+    {value: '2', label: '高'}
   ]
 
   const priorityKeyValue = priorityOptions.reduce((acc, cur) => {
@@ -233,10 +241,10 @@
 
   // 重要程度
   const importanceOptions = [
-    { value: '0', label: '提示' },
-    { value: '1', label: '一般' },
-    { value: '2', label: '重要' },
-    { value: '3', label: '关键' }
+    {value: '0', label: '提示'},
+    {value: '1', label: '一般'},
+    {value: '2', label: '重要'},
+    {value: '3', label: '关键'}
   ]
 
   const importanceKeyValue = importanceOptions.reduce((acc, cur) => {
@@ -246,8 +254,8 @@
 
   export default {
     name: 'Backlog',
-    components: { Pagination },
-    directives: { waves },
+    components: {Pagination},
+    directives: {waves},
     filters: {
       typeFilter(type) {
         return typeKeyValue[type]
@@ -312,33 +320,35 @@
         sprintOptions: null,
         priorityOptions,
         typeOptions: [
-          { value: '0', label: 'story' },
-          { value: '1', label: 'bug' }
+          {value: '0', label: 'story'},
+          {value: '1', label: 'bug'}
         ],
         priorityOrderOptions: [
-          { value: '1', label: '1' },
-          { value: '2', label: '2' },
-          { value: '3', label: '3' },
-          { value: '4', label: '4' },
-          { value: '5', label: '5' },
-          { value: '6', label: '6' },
-          { value: '7', label: '7' },
-          { value: '8', label: '8' },
-          { value: '9', label: '9' },
-          { value: '10', label: '10' }
+          {value: '1', label: '1'},
+          {value: '2', label: '2'},
+          {value: '3', label: '3'},
+          {value: '4', label: '4'},
+          {value: '5', label: '5'},
+          {value: '6', label: '6'},
+          {value: '7', label: '7'},
+          {value: '8', label: '8'},
+          {value: '9', label: '9'},
+          {value: '10', label: '10'}
         ],
 
         importanceOptions: [
-          { value: '0', label: '提示' },
-          { value: '1', label: '一般' },
-          { value: '2', label: '重要' },
-          { value: '3', label: '关键' }
+          {value: '0', label: '提示'},
+          {value: '1', label: '一般'},
+          {value: '2', label: '重要'},
+          {value: '3', label: '关键'}
         ],
         createUserRules: {
-          name: [{ required: true, trigger: 'blur', validator: validateUsername }],
-          phone: [{ required: true, trigger: 'blur', validator: validatePhone }],
-          mail: [{ required: true, trigger: 'blur', validator: validateMail }]
+          name: [{required: true, trigger: 'blur', validator: validateUsername}],
+          phone: [{required: true, trigger: 'blur', validator: validatePhone}],
+          mail: [{required: true, trigger: 'blur', validator: validateMail}]
         },
+        showIsLinkSprint: false,
+        linkSprintId: '',
         tableKey: 0,
         pvData: [],
         list: null,
@@ -371,6 +381,39 @@
       this.getList()
     },
     methods: {
+      refreshBeginAndEndDate(sprintId) {
+        this.sprintOptions.filter(item => {
+          if (item.id === sprintId) {
+            this.temp.beginDate = item.beginDate
+            this.temp.endDate = item.endDate
+          }
+        })
+      },
+      choseSprint(sprintId) {
+        if (sprintId) {
+          this.showIsLinkSprint = true
+          this.linkSprintId = sprintId
+          if (this.temp.isLinkSprint === '1') {
+            // 需要联动，更改为新选中迭代的开始日期和结束日期
+            this.refreshBeginAndEndDate(sprintId)
+          }
+        } else {
+          this.showIsLinkSprint = false
+          this.temp.isLinkSprint = '0'
+          this.temp.beginDate = ''
+          this.temp.endDate = ''
+        }
+      },
+      linkSprint(isLinkSprint) {
+        if (isLinkSprint === '0') {
+          // 不联动
+          this.temp.beginDate = ''
+          this.temp.endDate = ''
+        } else {
+          // 联动
+          this.refreshBeginAndEndDate(this.linkSprintId)
+        }
+      },
       getList() {
         this.listLoading = true
         fetchBacklogList(this.listQuery).then(response => {
@@ -387,13 +430,17 @@
       resetTemp() {
         this.temp = {
           title: '',
+          type: '0',
           statusId: '',
           currentUserId: '',
           moduleId: '',
           sprintId: '',
+          isLinkSprint: '0',
           priorityOrder: '',
           priority: '',
-          importance: ''
+          importance: '',
+          beginDate: '',
+          endDate: ''
         }
       },
       initTemp(row) {
@@ -433,7 +480,7 @@
           cancelButtonText: '否',
           type: 'error'
         })
-          .then(async() => {
+          .then(async () => {
             this.userId = row.id
             deleteUser(row.id).then(response => {
               console.log(response)
